14488f3106295fc98c4f792710566c3ba28c2057
Yoo <0620dadada@suwon.ac.kr>
2024-09-15 12:00:00 +0900

    코딩 테스트 연습: 2024-09-15 - 커밋 7

--- main.cpp
+++ main.cpp
@@ -9518,3 +9518,10 @@ int main() {
     return 0;
 }
 
+// 연습 날짜: 2024-09-15 - 커밋 7
+#include <iostream>
+int main() {
+    std::cout << "Hello, C++! Commit 7 on 2024-09-15" << std::endl;
+    return 0;
+}
+
